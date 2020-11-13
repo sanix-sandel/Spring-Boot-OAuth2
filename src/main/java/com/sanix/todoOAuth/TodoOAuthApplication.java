0e64c75ae656bd8b3d7d7e3f808b9110a344f609
sanix <sanicksikani@gmail.com>
2020-11-14 02:31:21 +0300

    initial commit

---
+++ src/main/java/com/sanix/todoOAuth/TodoOAuthApplication.java
@@ -0,0 +1,13 @@
+package com.sanix.todoOAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoOAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoOAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sanix/todoOAuth/TodoOAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanix.todoOAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoOAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
